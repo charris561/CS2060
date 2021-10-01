a766a86d678d92b9c78c66f1104872e195a4e18f
charris561 <charris4@uccs.edu>
2021-10-01 10:49:00 -0600

    uwu

---
+++ CS2060_Homework/A05-Grade_Calculator/AO5_Grades_Calculator_Source.c
@@ -0,0 +1,33 @@
+/*
+* Author: Caleb Harris
+* Class: CS2060 - 001
+* Assignment: A05 - Grades Calculator
+* 
+* Problem statement:
+* This program will calculate the grades of the user based on 
+* grading scheme on our canvas course. 
+*/
+
+//preprocessor
+#include <stdlib.h>
+#include <stdio.h>
+
+//constants
+const int NUM_STUDENTS = 10;
+
+int main(void) {
+
+	//prompt user to enter grades and store in array
+	//initialize array
+	int grades[10];//see constant can be used later
+
+	//walk the array and enter grades
+	
+
+}//main
+
+void enterGrades(int grades[]) {
+
+	for (size_t i = 0; i < NUM_STUDENTS; )
+
+}//enterGrades
